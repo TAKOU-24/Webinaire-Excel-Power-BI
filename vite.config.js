447419ba13c65fa4457136a0d0b3cfb04d272a85
TAKOU-24 <takoudimitri@yahoo.fr>
2026-08-10 21:09:17 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,6 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         merci: resolve(__dirname, "merci.html"),
-        inscription: resolve(__dirname, "inscription.html"),
-        merciInscription: resolve(__dirname, "merci-inscription.html"),
       },
     },
   },
